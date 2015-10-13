869dcbc2f77ca2ad0cf154290eb6f483780c4bb6
liurq <phpgege@gmail.com>
2015-10-13 08:37:08 +0800

    33

---
+++ test/js/resource.js
@@ -0,0 +1,14 @@
+/**
+ * Created by liurongqing on 10/12/15.
+ * email: phpgege@163.com
+ */
+
+var g_resources = [];
+
+var res_images = {
+
+};
+
+for(var i in res_images){
+    g_resources.push(res_images[i]);
+}
--- test/main.js
+++ test/main.js
@@ -2,3 +2,15 @@
  * Created by adin on 10/12/15
  * email: phpgege@163.com
  */
+cc.game.onStart = function () {
+    cc.view.enableAutoFullScreen(false);
+
+    var designSize = cc.size(480, 320);
+    cc.loader.resPath = "res/image";
+    cc.LoaderScene.preload(g_resources,function(){
+        cc.log(123);
+        console.log(456);
+    },this);
+};
+
+cc.game.run();
